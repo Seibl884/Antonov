87791ada20edc4c47df3d50c8c8b6b918508ac59
Seibl884 <antonovanton338@gmail.com>
2025-06-04 23:35:09 +0300

    Сделал 5_1

--- Lesson5.py
+++ Lesson5.py
@@ -1,64 +1,59 @@
-# Exists some car data with color, year, engine_volume, car type , price
-# We have search_criteria as tuple of ( year>= , engine_volume >= , price<=)
-# write code that will help us to get cars that satisfy search_criteria.
-# Cars should be sorted by price ascending.
-# We should print up to five (5) first found elements
+from collections import namedtuple
 
-car_data = {
-  'Mercedes': ('silver', 2019, 1.8, 'sedan', 50000),
-  'Audi': ('black', 2020, 2.0, 'sedan', 55000),
-  'BMW': ('white', 2018, 3.0, 'suv', 70000),
-  'Lexus': ('gray', 2016, 2.5, 'coupe', 45000),
-  'Toyota': ('blue', 2021, 1.6, 'hatchback', 25000),
-  'Honda': ('red', 2017, 1.5, 'sedan', 30000),
-  'Ford': ('green', 2019, 2.3, 'suv', 40000),
-  'Chevrolet': ('purple', 2020, 1.4, 'hatchback', 22000),
-  'Nissan': ('pink', 2018, 1.8, 'sedan', 35000),
-  'Volkswagen': ('brown', 2021, 1.4, 'hatchback', 28000),
-  'Hyundai': ('gray', 2019, 1.6, 'suv', 32000),
-  'Kia': ('white', 2020, 2.0, 'sedan', 28000),
-  'Volvo': ('silver', 2017, 1.8, 'suv', 45000),
-  'Subaru': ('blue', 2018, 2.5, 'wagon', 35000),
-  'Mazda': ('red', 2019, 2.5, 'sedan', 32000),
-  'Porsche': ('black', 2017, 3.0, 'coupe', 80000),
-  'Jeep': ('green', 2021, 3.0, 'suv', 50000),
-  'Chrysler': ('gray', 2016, 2.4, 'sedan', 22000),
-  'Dodge': ('yellow', 2020, 3.6, 'suv', 40000),
-  'Ferrari': ('red', 2019, 4.0, 'coupe', 500000),
-  'Lamborghini': ('orange', 2021, 5.0, 'coupe', 800000),
-  'Maserati': ('blue', 2018, 4.7, 'coupe', 100000),
-  'Bugatti': ('black', 2020, 8.0, 'coupe', 2000000),
-  'McLaren': ('yellow', 2017, 4.0, 'coupe', 700000),
-  'Rolls-Royce': ('white', 2019, 6.8, 'sedan', 500000),
-  'Bentley': ('gray', 2020, 4.0, 'coupe', 300000),
-  'Jaguar': ('red', 2016, 2.0, 'suv', 40000),
-  'Land Rover': ('green', 2018, 3.0, 'suv', 60000),
-  'Tesla': ('silver', 2020, 0.0, 'sedan', 60000),
-  'Acura': ('white', 2017, 2.4, 'suv', 40000),
-  'Cadillac': ('black', 2019, 3.6, 'suv', 55000),
-  'Infiniti': ('gray', 2018, 2.0, 'sedan', 35000),
-  'Lincoln': ('white', 2021, 2.0, 'suv', 50000),
-  'GMC': ('blue', 2016, 1.5, 'pickup', 30000),
-  'Ram': ('black', 2019, 5.7, 'pickup', 40000),
-  'Chevy': ('red', 2017, 2.4, 'pickup', 35000),
-  'Dodge Ram': ('white', 2020, 3.6, 'pickup', 45000),
-  'Ford F-Series': ('gray', 2021, 3.5, 'pickup', 50000),
-  'Nissan Titan': ('silver', 2018, 5.6, 'pickup', 35000)
+# Определяем структуру для данных автомобиля
+Car = namedtuple('Car', ['color', 'year', 'engine_volume', 'car_type', 'price'])
+
+car_data_namedtuple = {
+    'Mercedes': Car('silver', 2019, 1.8, 'sedan', 50000),
+    'Audi': Car('black', 2020, 2.0, 'sedan', 55000),
+    # ... и так далее для остальных машин
+    'Nissan': Car('pink', 2018, 1.8, 'sedan', 35000),
+    'Volkswagen': Car('brown', 2021, 1.4, 'hatchback', 28000),
+    'Hyundai': Car('gray', 2019, 1.6, 'suv', 32000),
+    'Kia': Car('white', 2020, 2.0, 'sedan', 28000),
+    'Volvo': Car('silver', 2017, 1.8, 'suv', 45000),
+    'Subaru': Car('blue', 2018, 2.5, 'wagon', 35000),
+    'Mazda': Car('red', 2019, 2.5, 'sedan', 32000),
+    'Porsche': Car('black', 2017, 3.0, 'coupe', 80000),
+    'Jeep': Car('green', 2021, 3.0, 'suv', 50000),
+    'Chrysler': Car('gray', 2016, 2.4, 'sedan', 22000),
+    'Dodge': Car('yellow', 2020, 3.6, 'suv', 40000),
+    'Ferrari': Car('red', 2019, 4.0, 'coupe', 500000),
+    'Lamborghini': Car('orange', 2021, 5.0, 'coupe', 800000),
+    'Maserati': Car('blue', 2018, 4.7, 'coupe', 100000),
+    'Bugatti': Car('black', 2020, 8.0, 'coupe', 2000000),
+    'McLaren': Car('yellow', 2017, 4.0, 'coupe', 700000),
+    'Rolls-Royce': Car('white', 2019, 6.8, 'sedan', 500000),
+    'Bentley': Car('gray', 2020, 4.0, 'coupe', 300000),
+    'Jaguar': Car('red', 2016, 2.0, 'suv', 40000),
+    'Land Rover': Car('green', 2018, 3.0, 'suv', 60000),
+    'Tesla': Car('silver', 2020, 0.0, 'sedan', 60000),
+    'Acura': Car('white', 2017, 2.4, 'suv', 40000),
+    'Cadillac': Car('black', 2019, 3.6, 'suv', 55000),
+    'Infiniti': Car('gray', 2018, 2.0, 'sedan', 35000),
+    'Lincoln': Car('white', 2021, 2.0, 'suv', 50000),
+    'GMC': Car('blue', 2016, 1.5, 'pickup', 30000),
+    'Ram': Car('black', 2019, 5.7, 'pickup', 40000),
+    'Chevy': Car('red', 2017, 2.4, 'pickup', 35000),
+    'Dodge Ram': Car('white', 2020, 3.6, 'pickup', 45000),
+    'Ford F-Series': Car('gray', 2021, 3.5, 'pickup', 50000),
+    'Nissan Titan': Car('silver', 2018, 5.6, 'pickup', 35000)
 }
-search_criteria = (2017, 1.6, 36000)
 
-# year >=, engine_volume >=, price <=
-filtered = [
-    (name, data)
-    for name, data in car_data.items()
-    if data[1] >= search_criteria[0]
-    and data[2] >= search_criteria[1]
-    and data[4] <= search_criteria[2]
+# Критерии поиска
+SearchCriteria = namedtuple('SearchCriteria', ['min_year', 'min_engine_volume', 'max_price'])
+search_criteria_namedtuple = SearchCriteria(2017, 1.6, 36000)
+
+filtered_namedtuple = [
+    (name, car)
+    for name, car in car_data_namedtuple.items()
+    if car.year >= search_criteria_namedtuple.min_year
+    and car.engine_volume >= search_criteria_namedtuple.min_engine_volume
+    and car.price <= search_criteria_namedtuple.max_price
 ]
 
-# sort by price ascending
-filtered_sorted = sorted(filtered, key=lambda x: x[1][4])
+filtered_sorted_namedtuple = sorted(filtered_namedtuple, key=lambda x: x[1].price)
 
-# print up to 5 first found elements
-for name, data in filtered_sorted[:5]:
-    print(f"{name}: {data}")
+print("\n--- Результаты с использованием namedtuple ---")
+for name, car in filtered_sorted_namedtuple[:5]:
+    print(f"{name}: {car}")
